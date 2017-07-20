55b4716b7cae1188d0a897daf4cac224d6ef11be
Oscar Corredor <oskar132@gmail.com>
2017-07-20 14:23:31 +0200

    getting started tutorial completed

---
+++ polling/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/odco/Desktop/git/django/polling/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ polling/mysite/polls/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    # attempt to retrieve the choice
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {'question':question, 'error_message': 'Please select a correct choice.'}
+        return render(request, 'polls/details.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+# # Create your views here.
+# def index(request):    
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template("polls/index.html")
+#     context = {
+#         'latest_question_list' : latest_question_list 
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, questionS_id):
+#     # try:
+#     #     question = Question.objects.get(pk=questionS_id)
+#     #     context = {'question':question}        
+#     #     return render(request, 'polls/details.html', context)
+#     # except:
+#     #     raise Http404("Question not found")
+#     question = get_object_or_404(Question, pk=questionS_id)
+#     context = {'question':question}
+#     return render(request, 'polls/details.html', context)
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html',{'question':question})
+#     return HttpResponse(response % question_id)
+
+# def vote(request, question_id):
+#     # attempt to retrieve the choice
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         context = {'question':question, 'error_message': 'Please select a correct choice.'}
+#         return render(request, 'polls/details.html', context)
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
